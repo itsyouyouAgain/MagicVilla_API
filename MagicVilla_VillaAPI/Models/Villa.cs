e1f52bd88281b7609716747e0e944857d4f4d506
itsyouyouAgain <qing.yo@northeastern.edu>
2024-03-27 21:56:32 -0700

    Initial commit

---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace MagicVilla_VillaAPI.Models
+{
+	public class Villa
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+        public string Details { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Amenity { get; set; }
+
+        public double Rate { get; set; }
+
+        public int Sqft { get; set; }
+
+        public int Occupancy { get; set; }
+
+        public DateTime CreatedDate{ get; set; }
+
+        public DateTime UpdateDate{ get; set; }
+
+    }
+}
+
